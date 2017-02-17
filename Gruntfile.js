8419c3c269b863aeba49f8babc4a4d22a45282c4
Derek Johnston <derekjohnston806@gmail.com>
2017-02-17 09:09:59 -0600

    Adding uglify and watch to grunt tasks

--- Gruntfile.js
+++ Gruntfile.js
@@ -11,9 +11,41 @@ module.exports = function (grunt) {
   // Project configuration
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
+    watch: {
+      src: {
+        files: 'src/**/*.js',
+        tasks: ["start"],
+      },
+      options: {
+        interrupt: true
+      }
+    },
+    uglify: {
+      dist: {
+        files: {
+          'public/scripts/bundle.min.js': ['public/scripts/bundle.js']
+        }
+      }
+    },
+    babel: {
+      options: {
+        sourceMap: true,
+        presets: ["latest", "react"]
+      },
+      dist: {
+        files: [
+          {
+            expand: true,
+            cwd: 'src/',
+            src: ['*.js'],
+            dest: 'dist/'
+          }
+        ]
+      }
+    },
     webpack: {
       default: {
-        entry: "./src/client.js",
+        entry: "./dist/client.js",
         output: {
           path    : "public/scripts/",
           filename: "bundle.js"
@@ -22,8 +54,11 @@ module.exports = function (grunt) {
     }
   });
 
+  grunt.loadNpmTasks("grunt-babel");
   grunt.loadNpmTasks("grunt-webpack");
+  grunt.loadNpmTasks("grunt-contrib-uglify");
+  grunt.loadNpmTasks("grunt-contrib-watch");
 
-  grunt.registerTask("default", ["webpack"]);
-
+  grunt.registerTask("start", ["babel", "webpack", "uglify"]);
+  grunt.registerTask("default", ["watch"]);
 };
